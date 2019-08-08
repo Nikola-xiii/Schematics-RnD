2b9fa6083f5137a9068769ca40c01684540eec6a
Nikolay <nserdiuk@oneglobus.com>
2019-08-08 18:02:03 +0300

    Added tpl for api module schematicts

--- api-module/src/api-module/index.ts
+++ api-module/src/api-module/index.ts
@@ -1,10 +1,15 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, template, mergeWith, url } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function apiModule(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    const sourceTpl = url('./templates');
+    const sourceParamsTpl = apply(sourceTpl, [template({ ..._options, ...strings})]);
+    console.log(tree);
+    
+    return mergeWith(sourceParamsTpl);
   };
 }
--- hello/src/hello/index.ts
+++ hello/src/hello/index.ts
@@ -1,13 +1,14 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, mergeWith, template } from '@angular-devkit/schematics';
 import { Schema } from './schema';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function hello(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const { name } = _options;
-    tree.create('hello.js', `console.log('Hello ${name}!');`);
-    return tree;
+    const sourceTpl = url('./templates');
+    const sourceParamsTpl = apply(sourceTpl, [template({ ..._options, ...strings})]);
+    return mergeWith(sourceParamsTpl);
   };
 }
